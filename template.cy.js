fff29be341f3658b4b2d8f8d83a6493faac0a646
Juan <juantreppo@hotmail.com>
2023-01-22 16:39:02 -0300

    template

--- template.cy.js
+++ template.cy.js
@@ -6,6 +6,6 @@ describe('TITULO DEL PACKETE DE PRUEBAS', () => {
     it('Test validar el titulo', () => {                
         cy.visit('https://demoqa.com/text-box');
         cy.title().should('eq','Upload Files | TestingQaRvn');      
-        cy.log('Todo ok, modificado desde git');                              
+        cy.log('Todo ok');                              
     })
 });
